9d0e967fb5d64b935e1dcd05838fb38edae41a14
Roberto Magale <roberto.magale@kobaltmusic.com>
2015-10-19 17:06:43 +0100

    Delete unused resource bundle

--- src/main/java/com/chipcollector/util/MessagesHelper.java
+++ src/main/java/com/chipcollector/util/MessagesHelper.java
@@ -1,27 +1,20 @@
 package com.chipcollector.util;
 
 import java.text.MessageFormat;
-import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class MessagesHelper {
-    private static final String BUNDLE_NAME = "com/chipcollector/resources/LabelMessages";
+    private static final String BUNDLE_NAME = "com/chipcollector/resources/DisplayStrings";
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
-    
-    private MessagesHelper() { }
-    
-    public static String getString(String key) {
-        try {
-            return RESOURCE_BUNDLE.getString(key);
-        } catch (MissingResourceException e) {
-        	return "?--" + key + "--?";
-        }
+
+    private MessagesHelper() {
     }
-    public static String getString(String key, Object... params  ) {
-        try {
-            return MessageFormat.format(RESOURCE_BUNDLE.getString(key), params);
-        } catch (MissingResourceException e) {
-            return "?--" + key + "--?";
-        }
+
+    public static String getString(String key) {
+        return RESOURCE_BUNDLE.getString(key);
+    }
+
+    public static String getString(String key, Object... params) {
+        return MessageFormat.format(RESOURCE_BUNDLE.getString(key), params);
     }
 }
