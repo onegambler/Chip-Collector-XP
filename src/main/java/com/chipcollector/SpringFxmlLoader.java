2e714f50f3b75fadd798bf105cd5142504d27905
Jenny <jenny.osborne@outlook.com>
2015-11-14 13:07:20 +0000

    double clicking the poker chip result launches the new poker chip window

--- src/main/java/com/chipcollector/SpringFxmlLoader.java
+++ src/main/java/com/chipcollector/SpringFxmlLoader.java
@@ -1,16 +1,25 @@
 package com.chipcollector;
 
+import com.chipcollector.controllers.dashboard.PokerChipDialogController;
 import com.google.common.base.Throwables;
 import com.google.common.io.Resources;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+import static com.chipcollector.controllers.dashboard.DashboardController.POKER_CHIP_ADD_DIALOG_FX_FILE_LOCATION;
 
 @Component
 public class SpringFxmlLoader implements ApplicationContextAware {
@@ -21,14 +30,38 @@ public class SpringFxmlLoader implements ApplicationContextAware {
 
     public <T> T load(String url) {
         try {
-            return FXMLLoader.load(Resources.getResource(url), bundle, null, applicationContext::getBean);
+            return getLoader(url).load();
         } catch (IOException e) {
             throw Throwables.propagate(e);
         }
     }
 
+    public FXMLLoader getLoader(String url) {
+        return new FXMLLoader(Resources.getResource(url), bundle, null, applicationContext::getBean);
+    }
+
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
+    }
+
+    public <T> void showDialog(String dialogFile, String title, Consumer<T> consumer) {
+
+        FXMLLoader loader = getLoader(dialogFile);
+        Parent dialog = null;
+        try {
+            dialog = loader.load();
+        } catch (IOException e) {
+            Throwables.propagate(e);
+        }
+        consumer.accept(loader.getController());
+
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle(title);
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+        //dialogStage.initOwner(); //TODO: init the owner
+        Scene scene = new Scene(dialog);
+        dialogStage.setScene(scene);
+        dialogStage.showAndWait();
     }
 }
--- src/main/java/com/chipcollector/controllers/dashboard/SearchPokerChipDialogController.java
+++ src/main/java/com/chipcollector/controllers/dashboard/SearchPokerChipDialogController.java
@@ -1,18 +1,24 @@
 package com.chipcollector.controllers.dashboard;
 
+import com.chipcollector.SpringFxmlLoader;
 import com.chipcollector.models.dashboard.CasinoBean;
 import com.chipcollector.models.dashboard.PokerChipBean;
 import com.chipcollector.scraper.ScraperEngine;
-import com.chipcollector.util.EventUtils;
-import com.sun.javafx.event.EventUtil;
+import com.google.common.base.Throwables;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -22,11 +28,10 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import static com.chipcollector.controllers.dashboard.DashboardController.POKER_CHIP_ADD_DIALOG_FX_FILE_LOCATION;
 import static com.chipcollector.util.EventUtils.isKeyboardEnterPressed;
 import static com.chipcollector.util.EventUtils.isMousePrimaryButtonPressed;
 import static java.util.Objects.nonNull;
-import static javafx.scene.input.KeyCode.ENTER;
-import static javafx.scene.input.MouseButton.PRIMARY;
 
 @Controller
 public class SearchPokerChipDialogController implements Initializable {
@@ -45,12 +50,14 @@ public class SearchPokerChipDialogController implements Initializable {
 
     //TODO: selezione sulla base della combobox
     private ScraperEngine engine;
+    private final SpringFxmlLoader springFxmlLoader;
 
     private CasinoBean currentlySelectedCasino;
 
     @Autowired
-    public SearchPokerChipDialogController(@Qualifier("theMogh") ScraperEngine engine) {
+    public SearchPokerChipDialogController(@Qualifier("theMogh") ScraperEngine engine, SpringFxmlLoader springFxmlLoader) {
         this.engine = engine;
+        this.springFxmlLoader = springFxmlLoader;
     }
 
     @Override
@@ -94,5 +101,22 @@ public class SearchPokerChipDialogController implements Initializable {
         if (isKeyboardEnterPressed(event)) {
             searchCasino();
         }
+    }
+
+    public void onPokerChipElementClicked(MouseEvent event) {
+        PokerChipBean selectedPokerChip = pokerChipsTableView.getSelectionModel().getSelectedItem();
+        if (nonNull(selectedPokerChip)) {
+            if (isMousePrimaryButtonPressed(event) && isMouseDoubleClicked(event)) {
+                openNewPokerChipDialog(selectedPokerChip);
+            }
+        }
+    }
+
+    private void openNewPokerChipDialog(PokerChipBean selectedPokerChip) {
+              springFxmlLoader.<PokerChipDialogController>showDialog(POKER_CHIP_ADD_DIALOG_FX_FILE_LOCATION, "Add new Poker Chip", controller -> controller.setPokerChipBean(selectedPokerChip));
+    }
+
+    private boolean isMouseDoubleClicked(MouseEvent event) {
+        return event.getClickCount() == 2;
     }
 }
